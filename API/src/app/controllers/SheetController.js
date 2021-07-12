031e0b0fee3340087c310da94c2f829709a1e9ff
Lucas Sônego <lucassonego@ufpr.br>
2021-07-12 19:59:00 -0300

    :bug: fix: Datas inseridas no formato correto no XLSX

--- API/src/app/controllers/SheetController.js
+++ API/src/app/controllers/SheetController.js
@@ -1,7 +1,11 @@
 import Excel from "exceljs";
 import path from "path";
 
-let cells = {
+const DAY = 0;
+const MONTH = 1;
+const YEAR = 2;
+
+const cells = {
   regionalDeSaude: "B2",
   populacao: "C2",
   casos0: "D2",
@@ -28,6 +32,19 @@ class SheetController {
             .replace("_", "/")
         );
 
+        let splitDate0 = `${data.offsetReport.dataValues.dia}`.split("_");
+        let data0 = new Date(
+          `${splitDate0[YEAR]}-${splitDate0[MONTH]}-${splitDate0[DAY]}`
+        );
+
+        let splitDate1 = `${data.dateReport.dataValues.dia}`.split("_");
+        let data1 = new Date(
+          `${splitDate1[YEAR]}-${splitDate1[MONTH]}-${splitDate1[DAY]}`
+        );
+
+        console.log(data0);
+        console.log(data1);
+
         worksheet.getCell(cells.regionalDeSaude).value = data.cityData.nome;
         worksheet.getCell(cells.populacao).value = data.cityData.populacao;
         worksheet.getCell(cells.casos0).value = data.offsetReport.casos;
@@ -38,14 +55,8 @@ class SheetController {
           data.offsetReport.recuperados;
         worksheet.getCell(cells.recuperados1).value =
           data.dateReport.recuperados;
-        worksheet.getCell(cells.data0).value =
-          `${data.offsetReport.dataValues.dia}`
-            .replace("_", "/")
-            .replace("_", "/");
-        worksheet.getCell(cells.data1).value =
-          `${data.dateReport.dataValues.dia}`
-            .replace("_", "/")
-            .replace("_", "/");
+        worksheet.getCell(cells.data0).value = data0;
+        worksheet.getCell(cells.data1).value = data1;
         worksheet.getCell(cells.offset).value = data.offset;
 
         return workbook.xlsx.writeFile(
